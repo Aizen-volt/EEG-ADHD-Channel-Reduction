a7696084ea5a2fae8532252006a5a25f6f619c7c
Mateusz Kaszubowski <m.aizenvolt@gmail.com>
2026-03-22 16:44:42 +0100

    Init project structure

---
+++ src/config.py
@@ -0,0 +1,57 @@
+﻿import logging
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATA_DIR = BASE_DIR / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+MODELS_DIR = BASE_DIR / "models"
+LOGS_DIR = BASE_DIR / "logs"
+
+NOTEBOOKS_DIR = BASE_DIR / "notebooks"
+REPORTS_DIR = BASE_DIR / "reports"
+FIGURES_DIR = REPORTS_DIR / "figures"
+SRC_DIR = BASE_DIR / "src"
+
+DIRECTORIES_TO_CREATE = [
+    RAW_DATA_DIR,
+    INTERIM_DATA_DIR,
+    PROCESSED_DATA_DIR,
+    MODELS_DIR,
+    NOTEBOOKS_DIR,
+    FIGURES_DIR,
+    SRC_DIR,
+]
+
+
+# --- Logger ---
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+def get_logger(name="EEG_ADHD"):
+    logger = logging.getLogger(name)
+
+    if not logger.hasHandlers():
+        logger.setLevel(logging.INFO)
+
+        formatter = logging.Formatter(
+            fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+
+        file_handler = logging.FileHandler(LOGS_DIR / "project.log", encoding="utf-8")
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+logger = get_logger()
